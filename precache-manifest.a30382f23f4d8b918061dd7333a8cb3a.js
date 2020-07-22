dd40e51fc5aa05d2dd7fa33d4c1f8375094e0a41
Timofey <Bakkayaroy@gmail.com>
2020-07-22 04:33:34 +0300

    Updates

---
+++ precache-manifest.a30382f23f4d8b918061dd7333a8cb3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e61c540836e2aab1ecd08d99643804",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a8f5353328eca129e234",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "de1f9e1f66da2295b60f",
+    "url": "/portfolio/static/css/main.02c2a5d7.chunk.css"
+  },
+  {
+    "revision": "a8f5353328eca129e234",
+    "url": "/portfolio/static/js/2.6633c2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.6633c2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1f9e1f66da2295b60f",
+    "url": "/portfolio/static/js/main.639ebaf4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8e35b83cd420243b7e72af0066c7b534",
+    "url": "/portfolio/static/media/Doge.8e35b83c.jpg"
+  },
+  {
+    "revision": "00e66f7a00c54ac417bd50eff5241f21",
+    "url": "/portfolio/static/media/Photo.00e66f7a.png"
+  },
+  {
+    "revision": "764643497bfa25e4bc8c080b91abd607",
+    "url": "/portfolio/static/media/github-logo.76464349.jpg"
+  },
+  {
+    "revision": "da0489d7dc5836f9446a6ba6e7791fd8",
+    "url": "/portfolio/static/media/gosling.da0489d7.jpg"
+  },
+  {
+    "revision": "0b0f30fed58d36fdb97631d07aa939bd",
+    "url": "/portfolio/static/media/telegram.0b0f30fe.jpg"
+  }
+]);
